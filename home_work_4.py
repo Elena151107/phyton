8d37e27aed0a8cc7b6e99317446a4396133b2ba9
Elena151107 <162087911+Elena151107@users.noreply.github.com>
2024-09-10 17:15:13 +0700

    Create home_work_4.py

---
+++ home_work_4.py
@@ -0,0 +1,37 @@
+# home_work 4/1
+
+list1 = [(1, 2, 0), (3, 4), (5, 2, 4, 10), (7, 8)]
+print(list1)
+# 1
+list2 = [ i for elem in list1 for i in elem]
+print(list2)
+# 2
+for el in list1:
+    for i in el:
+        list2.append(i)
+
+#vhome_work 4/2
+
+list1 = [1, 2, 0, 3, '4', 5, 2, 4, 10, 7, 8]
+print(list1)
+target = 4
+list2 = [i for i in list1 if int(i) != target]
+print(list2)
+
+# home_work 4/3
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+list2 = [17, 18, 11]
+intersect_list = list(set(list1).intersection(list2))   # метод пересечения
+if intersect_list:
+    print(intersect_list)
+else:
+    print('no intersection')
+
+# home_work 4/4
+list1 = ['ab', 'a', 'abcdef', 'a', 'abcde', 'abc', 'abcd']
+max_len = 1
+for elem in range(len(list1)):
+    if len(list1[elem]) > max_len:
+        max_len = len(list1[elem])
+list2 = [word + '_' * (max_len - len(word)) for word in list1]
+print(list2)
