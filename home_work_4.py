d472878e59e08da62533857492e07151628412b5
Elena151107 <162087911+Elena151107@users.noreply.github.com>
2024-09-11 22:14:30 +0700

    Update home_work_4.py

--- home_work_4.py
+++ home_work_4.py
@@ -1,18 +1,24 @@
-# home_work 4/1
+# # home_work 4/1
+""" Напишите программу, которая принимает список, содержащий вложенные списки,
+ и возвращает новый список, содержащий все элементы из вложенных списков, без вложенности."""
 
 list1 = [(1, 2, 0), (3, 4), (5, 2, 4, 10), (7, 8)]
 print(list1)
-# 1
+
+# # 1
 list2 = [ i for elem in list1 for i in elem]
-print(list2)
-# 2
-for el in list1:
-    for i in el:
-        list2.append(i)
+print(list2)    #   [1, 2, 0, 3, 4, 5, 2, 4, 10, 7, 8]
+
+# # 2
+# list2 = []
+# for el in list1:
+#     for i in el:
+#         list2.append(i)
+# print(list2)   #   [1, 2, 0, 3, 4, 5, 2, 4, 10, 7, 8]
 
 
 
-#vhome_work 4/2
+# home_work 4/2
 
 list1 = [1, 2, 0, 3, '4', 5, 2, 4, 10, 7, 8]
 print(list1)
