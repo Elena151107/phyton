f709ef6774cd40d06b0f8ab439b9cf19471edaba
Elena151107 <162087911+Elena151107@users.noreply.github.com>
2024-09-11 22:30:34 +0700

    Update home_work_4.py

--- home_work_4.py
+++ home_work_4.py
@@ -1,4 +1,4 @@
-# # home_work 4/1
+# home_work 4/1
 """ Напишите программу, которая принимает список, содержащий вложенные списки,
  и возвращает новый список, содержащий все элементы из вложенных списков, без вложенности."""
 
@@ -19,38 +19,48 @@ print(list2)    #   [1, 2, 0, 3, 4, 5, 2, 4, 10, 7, 8]
 
 
 # home_work 4/2
+""" Напишите программу, которая принимает список и значение,
+и удаляет все вхождения этого значения из списка."""
 
 list1 = [1, 2, 0, 3, '4', 5, 2, 4, 10, 7, 8]
 print(list1)
 target = 4
-list2 = [i for i in list1 if int(i) != target]
-print(list2)
+list2 = [ i for i in list1 if int(i) != target]
+print(list2)    #   [1, 2, 0, 3, 5, 2, 10, 7, 8]
 
 
 
 # home_work 4/3
-
-list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-list2 = [17, 18, 11]
-intersect_list = list(set(list1).intersection(list2))   # метод пересечения
+""" Напишите программу, которая принимает два списка и возвращает True,
+если первый список является подмножеством второго списка, и False в противном случае."""
+list1 = [7, 18, 11]
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+intersect_list = list(set(list2).intersection(list1))   # метод пересечения
 if intersect_list:
-    print(intersect_list)
+    print('True')
 else:
-    print('no intersection')
+    print('False')
 
 
 
 # home_work 4/4
+""" На входе имеем список строк разной длины. Необходимо написать программу,
+которая вернет новый список из строк одинаковой длины. Длину итоговой строки
+определяем исходя из самой большой из них. Если конкретная строка короче самой длинной,
+дополнить ее нижними подчеркиваниями с правого края до требуемого количества символов.
+Расположение элементов начального списка не менять."""
 
 list1 = ['ab', 'a', 'abcdef', 'a', 'abcde', 'abc', 'abcd']
-# 1
+
+# # 1
 max_len_word = len(max(list1))
 list2 = [word + '_' * (max_len_word - len(word)) for word in list1]
-print(list2)
-# 2
+print(list2)         #   ['ab____', 'a_____', 'abcdef', 'a_____', 'abcde_', 'abc___', 'abcd__']
+
+# # 2
 max_len = 1
 for elem in range(len(list1)):
     if len(list1[elem]) > max_len:
         max_len = len(list1[elem])
 list2 = [word + '_' * (max_len - len(word)) for word in list1]
-print(list2)
+print(list2)        #    ['ab____', 'a_____', 'abcdef', 'a_____', 'abcde_', 'abc___', 'abcd__']
